87391188e9b912c13c9a38ecd983fcf09ff80267
Temilollu <temiloluwaodumosu25@gmail.com>
2020-05-05 10:58:21 +0100

    Updates

---
+++ precache-manifest.e36043c8ed10a95f83717789b766a426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6474e1a3d4a1c3349cc78fbfb4d8860",
+    "url": "/React-project/index.html"
+  },
+  {
+    "revision": "9f4bb6f94cb4d7ad6bd2",
+    "url": "/React-project/static/css/main.4a331d59.chunk.css"
+  },
+  {
+    "revision": "d8c3667197a380a746ef",
+    "url": "/React-project/static/js/2.b49dc79f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-project/static/js/2.b49dc79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f4bb6f94cb4d7ad6bd2",
+    "url": "/React-project/static/js/main.216dea92.chunk.js"
+  },
+  {
+    "revision": "0274f68fdc57e9f41c36",
+    "url": "/React-project/static/js/runtime-main.4206e127.js"
+  }
+]);
